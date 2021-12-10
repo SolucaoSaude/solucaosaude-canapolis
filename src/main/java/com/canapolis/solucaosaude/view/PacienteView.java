253f8b1edacbd63b3f6b1924dbd2fb6d25422143
solucaosaude <solucaosaude.canapolis@gmail.com>
2021-12-10 17:46:33 -0300

    correcao bug

--- src/main/java/com/canapolis/solucaosaude/view/PacienteView.java
+++ src/main/java/com/canapolis/solucaosaude/view/PacienteView.java
@@ -30,6 +30,11 @@ public class PacienteView extends BaseView {
     @Autowired
     private PacienteRepository pacienteRepository;
 
+    @RequestMapping(value = "/")
+    public String iniciar() {
+        return "tela1";
+    }
+
     @RequestMapping(value = "/novo-paciente", method = RequestMethod.GET)
     public String novoRegistro(Model model){
 
